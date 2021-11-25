ab65181d92e5d1e80c844733ac4ac1acc9c72331
Max1O <fcb10max@gmail.com>
2021-11-25 11:35:45 +0500

    url change

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
     const {  username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "http://localhost:5000/auth";
+    const URL = "https://server-chat-app0.herokuapp.com/auth";
 
     const { data: { token, userId, hashedPassword, fullName } } = await axios.post(
       `${URL}/${isSignUp ? "signup" : "login"}`,
